2485447dec025ade665d194d7bc41a80943b5dc3
becory <13202145+becory@users.noreply.github.com>
2021-10-06 18:54:34 +0800

    chore: set heroku postgres

--- src/config/config.js
+++ src/config/config.js
@@ -1,21 +1,25 @@
-module.exports =  {
-  "development": {
-    "username": "ssuyuanhuang",
-    "password": "",
-    "database": "docs-demo",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "salt": "a"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
-  "production": {
-    "use_env_variable": "DATABASE_URL",
-    "salt": process.env.SALT
-  }
+module.exports = {
+    "development": {
+        "username": "ssuyuanhuang",
+        "password": "",
+        "database": "docs-demo",
+        "host": "127.0.0.1",
+        "dialect": "postgres",
+        "salt": "a"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "postgres"
+    },
+    "production": {
+        "use_env_variable": "DATABASE_URL",
+        ssl: true,
+        dialectOptions: {
+            ssl: true,
+        },
+        "salt": process.env.SALT
+    }
 }
